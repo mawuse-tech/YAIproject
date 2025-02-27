8dac0b27781e0cecb9b48a9c2e33ead31d96a754
mawuse-tech <akpekemawuse1@gmail.com>
2025-02-27 09:01:53 +0000

    worked on the video

---
+++ index.js
@@ -0,0 +1,47 @@
+const serviceOne = document.getElementById('service1')
+const serviceBtnone = document.getElementById('service-btn1')
+const closeServiceOne = document.getElementById('close-service-1')
+
+const serviceTwo = document.getElementById('service2')
+const serviceBtnTwo = document.getElementById('service-btn2')
+const closeServicetwo = document.getElementById('close-service-2')
+
+const serviceThree = document.getElementById('service3')
+const serviceBtnThree = document.getElementById('service-btn3')
+const closeServiceThree = document.getElementById('close-service-3')
+
+const videoBtn = document.getElementById('vid-btn')
+const video = document.getElementById('video')
+
+
+
+serviceBtnone.addEventListener('click', function () {
+  serviceOne.style.display = 'inline'
+})
+
+closeServiceOne.addEventListener("click", function () {
+  serviceOne.style.display = 'none';
+})
+
+
+serviceBtnTwo.addEventListener('click', function () {
+  serviceTwo.style.display = 'inline'
+})
+
+closeServicetwo.addEventListener("click", function () {
+  serviceTwo.style.display = 'none';
+})
+
+
+serviceBtnThree.addEventListener('click', function () {
+  serviceThree.style.display = 'inline'
+})
+
+closeServiceThree.addEventListener("click", function () {
+  serviceThree.style.display = 'none';
+})
+
+videoBtn.addEventListener('click', function () {
+  video.style.display = 'inline'
+})
+
